3754d53be1f01485af9956f14c0ef8bab12074c2
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-21 01:17:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,12 @@ class Dog
   def bark
     puts "Woof!"
   end
+
+  def sit
+    puts "The Dog is sitting"
+  end
 end
 
 fido = Dog.new
 fido.bark #> "Woof!"
-fido.sit
+fido.sit #> "The Dog is sitting"
